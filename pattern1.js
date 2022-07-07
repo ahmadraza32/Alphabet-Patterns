d6b5e80f8997d15c396a99b6597d3e7438f327f9
ahmadraza32 <ahmedraza32125@gmail.com>
2022-07-07 02:28:38 -0700

    first commit

---
+++ pattern1.js
@@ -0,0 +1,13 @@
+
+for(let i=1; i<=5; i++){
+    for(let j=1; j<=i; j++){
+        document.write(String.fromCharCode(64 + j))
+    }
+    document.write("<br />")
+}
+
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
---
+++ pattern2.js
@@ -0,0 +1,13 @@
+
+for(let i=5; i>=1; i--){
+    for(let j=1; j<=i; j++){
+        document.write(String.fromCharCode(64 + j))
+    }
+    document.write("<br />")
+}
+
+// ABCDE
+// ABCD
+// ABC
+// AB
+// A
